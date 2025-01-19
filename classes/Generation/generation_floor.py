ed86becb6587329ea483626576b49a91a03f690b
Dmitrii <dsamojlov04@gmail.com>
2025-01-19 23:23:20 +0700

    Создана переменная EMPTY_MAP (Пустое поле), определенны базовые объекты (Пол, Стена и Дверь), а также класс Map, который будет использоваться для генерации карты - Первый метод - создание границ карты

--- classes/Generation/generation_floor.py
+++ classes/Generation/generation_floor.py
@@ -1,6 +1,47 @@
 import pygame
 import random
-
+from pprint import pprint
 
 SEED = "9 1 2 0 3 9 2 1 0 9 4 1 2"
 
+EMPTY_MAP = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+objects = {0: "floor",
+           1: "wall",
+           2: "door"}
+pprint(EMPTY_MAP)
+print()
+EMPTY_MAP[0][1] = 1
+pprint(EMPTY_MAP)
+
+
+class Map:
+    def __init__(self, list_map):
+        self.map = list_map
+        self.w, self.h = self.size = len(list_map[0]), len(list_map)
+
+    def make_board(self):
+        for y in range(self.h):
+            for x in range(self.w):
+                cord_cell = x, y
+                if x == 0 or y == 0 or x == self.w - 1 or y == self.h - 1:
+                    self.map[y][x] = 1
+        return self.map
+
+
+game = Map(EMPTY_MAP)
+pprint(game.make_board())
