72088d9f98bcab6f3211b6211a17eb40c6080f60
Dmitrii <dsamojlov04@gmail.com>
2025-01-20 18:52:20 +0700

    Новая функция draw_line Новая функция random_cord
    Возможно понадобиться пока что не написанная функция choice_cords

--- classes/Generation/generation_floor.py
+++ classes/Generation/generation_floor.py
@@ -27,14 +27,22 @@ objects = {0: "floor",
            1: "wall",
            2: "door"}
 
-# Мои тесты
-pprint(EMPTY_MAP)
-print()
-EMPTY_MAP[0][1] = 1
-pprint(EMPTY_MAP)
+
+# # Мои тесты
+# pprint(EMPTY_MAP)
+# print()
+# EMPTY_MAP[0][1] = 1
+# pprint(EMPTY_MAP)
 
 
 # Класс для создания (генерации) карты
+def random_cord(start_zero, fin_zero):
+    rand_cord = random.randint(start_zero, fin_zero)
+    while rand_cord < 4 or rand_cord > 11:
+        rand_cord = random.randint(start_zero, fin_zero)
+    return rand_cord
+
+
 class Map:
     def __init__(self, list_map):
         self.map = list_map
@@ -43,12 +51,38 @@ class Map:
     def make_board(self):
         for y in range(self.h):
             for x in range(self.w):
-                cord_cell = x, y
                 if x == 0 or y == 0 or x == self.w - 1 or y == self.h - 1:
                     self.map[y][x] = 1
+        return self.map
+
+    def draw_line(self, init_x, init_y, direction, count=0):
+        if direction == 0:
+            for x in range(init_x, 14 + 1):
+                self.map[init_y][x] = 1
+        else:
+            for x in range(init_x, 14 + 1):
+                self.map[x][init_y] = 1
+        # while count != 2:
+        #     count += 1
+        #     direction = 1 if direction == 0 else 0
+        #     if random.random():
+        #         cord = random_cord(init_x + 4, 11)
+        #     else:
+        #         cord = random_cord(init_x - 4, 4)
+        #     self.draw_line(0, cord, direction, count)
+        return self.map
+
+    def choice_cords(self):
+        pass
+
+    def __str__(self):
         return self.map
 
 
 # Тесты класса
 game = Map(EMPTY_MAP)
 pprint(game.make_board())
+print()
+
+rand = random_cord(3, 11)
+pprint(game.draw_line(0, rand, 1))
