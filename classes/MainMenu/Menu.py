7bfd1ac527e6d0e9ff8760d2942f5042abe87365
Alexboss <alexkolesnikov1b@mail.ru>
2025-01-21 19:31:42 +0700

    Working menu!!!

--- classes/MainMenu/Menu.py
+++ classes/MainMenu/Menu.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 import os
 
+
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
     try:
@@ -16,49 +17,60 @@ def load_image(name, color_key=None):
         image.set_colorkey(color_key)
     return image
 
+
 pygame.init()
 
 clock = pygame.time.Clock()
 
-window_size = (400, 400)
+window_size = (1600, 900)
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption('Pygame Кликабельная кнопка')
 
 font = pygame.font.Font(None, 24)
 
-button_surface = pygame.Surface((150, 50))
 
-text = font.render("Click Me", True, (0, 0, 0))
-text_rect = text.get_rect(
-    center=(button_surface.get_width() / 2,
-            button_surface.get_height() / 2))
+def draw_button(button_rect, text, hover):
+    button_surface = pygame.Surface((button_rect.width, button_rect.height))
+    button_surface.fill((0, 0, 0))
+    if hover:
+        pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, button_rect.width - 2, button_rect.height - 2))
+    else:
+        pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, button_rect.width - 2, button_rect.height - 2))
+        pygame.draw.rect(button_surface, (0, 0, 0), (1, 1, button_rect.width - 2, 1), 2)
+        pygame.draw.rect(button_surface, (0, 100, 0), (1, button_rect.height - 2, button_rect.width - 2, 10), 2)
 
-button_rect = pygame.Rect(125, 125, 150, 50)  # Менять положение кнопки
+    text_surface = font.render(text, True, (0, 0, 0))
+    text_rect = text_surface.get_rect(center=(button_rect.width / 2, button_rect.height / 2))
+    button_surface.blit(text_surface, text_rect)
+    screen.blit(button_surface, button_rect.topleft)
+
+
+#Рсположение и размеры кнопок
+button_rects = [
+    pygame.Rect(625, 400, 400, 60),
+    pygame.Rect(625, 300, 400, 60),
+    pygame.Rect(625, 500, 400, 60),
+    pygame.Rect(1345, 800, 200, 60)
+]
+
+button_texts = ["Продолжить", "Новая игра", "Настройки", "Выйти"]
 
 while True:
     clock.tick(60)
     fon = pygame.transform.scale(load_image('fon.jpg'), window_size)
-    screen.blit((fon), (0, 0))
+    screen.blit(fon, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if button_rect.collidepoint(event.pos):
-                print("Button clicked!")
+            for i, button_rect in enumerate(button_rects):
+                if button_rect.collidepoint(event.pos):
+                    print(f"{button_texts[i]} нажата!")
 
-    if button_rect.collidepoint(pygame.mouse.get_pos()):
-        pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, 148, 48))
-    else:
-        pygame.draw.rect(button_surface, (0, 0, 0), (0, 0, 150, 50))
-        pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, 148, 48))
-        pygame.draw.rect(button_surface, (0, 0, 0), (1, 1, 148, 1), 2)
-        pygame.draw.rect(button_surface, (0, 100, 0), (1, 48, 148, 10), 2)
-
-    button_surface.blit(text, text_rect)
-
-    screen.blit(button_surface, (button_rect.x, button_rect.y))
+    for i, button_rect in enumerate(button_rects):
+        hover = button_rect.collidepoint(pygame.mouse.get_pos())
+        draw_button(button_rect, button_texts[i], hover)
 
     pygame.display.update()
