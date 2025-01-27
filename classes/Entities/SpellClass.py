3934d907c298b8d9f16e8589573c5c1ca691c1a1
Ordo_max <gorobec8800@gmail.com>
2025-01-27 14:39:46 +0300

    + зависимость от переменной скорости

--- classes/Entities/SpellClass.py
+++ classes/Entities/SpellClass.py
@@ -14,10 +14,11 @@ class Spell(Entity):
         self.image = pygame.transform.scale(self.image, (20, 20))
         self.rect = self.image.get_rect(center=(0, 0))
         self.vec = (0, 0)
+        self.speed = 3
     def update(self, center=None, map_move=(0, 0)):
         if center != None:
             self.center = center[0] - map_move[0] + self.vec[0] * 25, center[1] - map_move[1] + self.vec[1] * 25
-        self.center = self.center[0] + self.vec[0], self.center[1] + self.vec[1]
+        self.center = self.center[0] + self.vec[0] * self.speed, self.center[1] + self.vec[1] * self.speed
         self.rect.center = self.center[0] + map_move[0], self.center[1] + map_move[1]
 
 
